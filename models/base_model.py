9fd2516ec2a5a127a8c0b34f94137d10fb49b385
jackeado <pabsanchez@gmail.com>
2020-02-14 17:36:42 -0500

    new correction

--- models/base_model.py
+++ models/base_model.py
@@ -7,26 +7,37 @@ from datetime import datetime
 
 
 class BaseModel:
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
     """A BaseModel class"""
-    def __init__(self, *args, *kargs):
+    def __init__(self, *args, **kwargs):
         """Initialize class base"""
         self.id = str(uuid4())
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs is not None:
+            for keys, value in kwargs.items():
+                if keys is not "__class__":
+                    if keys is "created_at" or keys is "updated_at":
+                        value = datetime.strptime(self.created_at.isoformat(), "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, keys, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """"""
-        return ("[{}] {} {}".format(self.__class__.__name__,
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
                                     self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute with the current datetime"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary """
-        newBase = self.__dict__.copy()
-        newBase["__class__"] = self.__class__.__name__
-        newBase["create_at"] = self.create_at
-        newBase["update_at"] = self.update_at
-        return newBase
+        newbase = self.__dict__.copy()
+        newbase.update({"__class__": self.__class__.__name__})
+        newbase.update({"created_at": self.created_at.isoformat()})
+        newbase.update({"updated_at": self.updated_at})
+        return newbase
---
+++ models/engine/file_storage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import json
+from models import BaseModel
+
+def __init__(self):
