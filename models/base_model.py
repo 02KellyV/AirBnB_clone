4ff9cd0403a8f033dfd122c7fac19d1e96d01462
jackeado <pabsanchez@gmail.com>
2020-02-19 20:06:06 -0500

    fix programs

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,16 @@
 """
 This contains a base class
 """
+
 from uuid import uuid4
 from datetime import datetime
 import models
 
 
-class BaseModel:
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
+
+class BaseModel:
 
     """A BaseModel class"""
     def __init__(self, *args, **kwargs):
@@ -17,7 +20,7 @@ class BaseModel:
             for keys, value in kwargs.items():
                 if keys != "__class__":
                     if keys == "created_at" or keys == "updated_at":
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value, time)
                     setattr(self, keys, value)
         else:
             self.id = str(uuid4())
@@ -25,11 +28,10 @@ class BaseModel:
             self.updated_at = self.created_at
             models.storage.new(self)
 
-
     def __str__(self):
         """print the class name, id and directory"""
         return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                    self.id, self.__dict__))
+                                      self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute with the current datetime"""
@@ -39,7 +41,7 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary """
         newbase = self.__dict__.copy()
-        newbase["__class__"] =self.__class__.__name__
-        newbase["created_at"]=self.created_at.isoformat()
-        newbase["updated_at"]=self.updated_at.isoformat()
+        newbase["__class__"] = self.__class__.__name__
+        newbase["created_at"] = self.created_at.isoformat()
+        newbase["updated_at"] = self.updated_at.isoformat()
         return newbase
---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class User
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Representation of a user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
