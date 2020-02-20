d69c805ab289ee61c082aaaa7a32987a8aa49a60
02KellyV <900@holbertonschool.com>
2020-02-20 03:42:17 +0000

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
-from models.engine.file_storage import FileStorage
-from models import base_model
+#!/usr/bin/python3
 """
 Contains the Storage
 """
+from models.engine.file_storage import FileStorage
+from models import base_model
 
 storage = FileStorage()
 storage.reload()
+
